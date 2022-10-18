f43d5bde7bbbb70d263bc83216ae92b5da364d66
ychamare <54070011+ychamare@users.noreply.github.com>
2022-10-18 08:17:58 -0600

    changes to timeout simulator

--- simulator.py
+++ simulator.py
@@ -126,22 +126,24 @@ logger.addHandler(streamHandler)
 
 # Init AWSIoTMQTTClient
 myAWSIoTMQTTClient = None
-myAWSIoTMQTTClient.configureMQTTOperationTimeout = 0
+
 if useWebsocket:
     myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId, useWebsocket=True)
     myAWSIoTMQTTClient.configureEndpoint(host, port)
     myAWSIoTMQTTClient.configureCredentials(rootCAPath)
+    
 else:
     myAWSIoTMQTTClient = AWSIoTMQTTClient(clientId)
     myAWSIoTMQTTClient.configureEndpoint(host, port)
     myAWSIoTMQTTClient.configureCredentials(rootCAPath, privateKeyPath, certificatePath)
+    
 
 # AWSIoTMQTTClient connection configuration
 myAWSIoTMQTTClient.configureAutoReconnectBackoffTime(1, 32, 20)
 myAWSIoTMQTTClient.configureOfflinePublishQueueing(-1)  # Infinite offline Publish queueing
 myAWSIoTMQTTClient.configureDrainingFrequency(2)  # Draining: 2 Hz
-myAWSIoTMQTTClient.configureConnectDisconnectTimeout(10)  # 10 sec
-myAWSIoTMQTTClient.configureMQTTOperationTimeout(5)  # 5 sec
+myAWSIoTMQTTClient.configureConnectDisconnectTimeout(20)  # 10 sec
+myAWSIoTMQTTClient.configureMQTTOperationTimeout(15)  # 5 sec
 
 # Connect and subscribe to AWS IoT
 myAWSIoTMQTTClient.connect()
@@ -864,7 +866,7 @@ while True:
      
         if args.mode == 'publish':
             print('Published topic %s: %s\n' % (topic1, messageJson1))
-        else:
+        
             
         loopCount += 1
     time.sleep(5)
